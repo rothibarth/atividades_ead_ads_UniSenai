e465487d43c6193d5939d444545a6095451270dd
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-12 19:19:17 -0300

    Finalizando a OT 01 Javascript Atualização

---
+++ JAVASCRIPT ATUALIZAÇÃO - OT 01 ATVD EAD/index.js
@@ -0,0 +1,5 @@
+let nome = "Felipe Rafael Rothbarth";
+let idade = "19 anos";
+let comida_favorita = "Strogonoff de frango";
+
+console.log("Meu nome é ", nome, "tenho", idade, "Minha comida favorita é ", comida_favorita);
